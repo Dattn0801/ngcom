eb3c62513fb50f172783294b188dd0caf15440d2
Dattn0801 <Dattn0801>
2022-03-28 21:28:09 +0700

    show_list_category

--- dcom/apps/admin/src/app/app.module.ts
+++ dcom/apps/admin/src/app/app.module.ts
@@ -4,35 +4,43 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-
+//App component
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
 
+//Primeng Component
 import { CardModule } from 'primeng/card';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
-//import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
-import { InputTextModule } from 'primeng/inputtext';
 
+const UX_MODULE = [CardModule, ToolbarModule, ButtonModule, TableModule];
 const routes: Routes = [
     {
         path: '',
         component: ShellComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent }
-            //{ path: 'categories', component: CategoriesListComponent },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'categories', component: CategoriesListComponent }
             //{ path: 'categories/form', component: CategoriesFormComponent }
         ]
     }
 ];
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        ShellComponent,
+        SidebarComponent,
+        CategoriesListComponent
+    ],
     imports: [
         BrowserModule,
         HttpClientModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+        ...UX_MODULE
     ],
     providers: [],
     bootstrap: [AppComponent]
