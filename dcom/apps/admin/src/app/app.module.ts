b7f56b5c042471b8c5dc144d22e87a7897555cec
Dattn0801 <Dattn0801>
2022-03-28 15:11:03 +0700

    router-admin

--- dcom/apps/admin/src/app/app.module.ts
+++ dcom/apps/admin/src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+//import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { InputTextModule } from 'primeng/inputtext';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent }
+            //{ path: 'categories', component: CategoriesListComponent },
+            //{ path: 'categories/form', component: CategoriesFormComponent }
+        ]
+    }
+];
 @NgModule({
     declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
