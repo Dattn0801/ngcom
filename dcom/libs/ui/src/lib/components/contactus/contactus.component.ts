2d386d0efe2cc650d218475647625d023715f382
Dattn0801 <Dattn0801>
2022-06-10 00:47:56 +0700

    add_contact_us

---
+++ dcom/libs/ui/src/lib/components/contactus/contactus.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ui-contactus',
+    templateUrl: './contactus.component.html',
+    styleUrls: ['./contactus.component.css']
+})
+export class ContactusComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
--- dcom/libs/ui/src/lib/ui.module.ts
+++ dcom/libs/ui/src/lib/ui.module.ts
@@ -4,9 +4,17 @@ import { BannerComponent } from './components/banner/banner.component';
 import { ButtonModule } from 'primeng/button';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { IncentiveComponent } from './components/incentive/incentive.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+    {
+        path: 'contact',
+        component: ContactusComponent
+    }
+];
 @NgModule({
-    imports: [CommonModule, ButtonModule],
-    declarations: [BannerComponent, GalleryComponent, IncentiveComponent],
-    exports: [BannerComponent, GalleryComponent, IncentiveComponent]
+    imports: [CommonModule, ButtonModule, RouterModule.forChild(routes)],
+    declarations: [BannerComponent, GalleryComponent, IncentiveComponent, ContactusComponent],
+    exports: [BannerComponent, GalleryComponent, IncentiveComponent, ContactusComponent]
 })
 export class UiModule {}
