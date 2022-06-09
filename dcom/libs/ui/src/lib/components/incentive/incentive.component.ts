68122a80ac6ce0f0315666adbfdc8de0a58438c9
Dattn0801 <Dattn0801>
2022-06-09 23:17:50 +0700

    refacter_home_page

---
+++ dcom/libs/ui/src/lib/components/incentive/incentive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-incentive',
+  templateUrl: './incentive.component.html',
+  styleUrls: ['./incentive.component.css']
+})
+export class IncentiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- dcom/libs/ui/src/lib/ui.module.ts
+++ dcom/libs/ui/src/lib/ui.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { ButtonModule } from 'primeng/button';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { IncentiveComponent } from './components/incentive/incentive.component';
 @NgModule({
     imports: [CommonModule, ButtonModule],
-    declarations: [BannerComponent, GalleryComponent],
-    exports: [BannerComponent, GalleryComponent]
+    declarations: [BannerComponent, GalleryComponent, IncentiveComponent],
+    exports: [BannerComponent, GalleryComponent, IncentiveComponent]
 })
 export class UiModule {}
