a607d59a1f7ceacbd580146f02b9d442387fdf65
Dattn0801 <Dattn0801>
2022-06-14 00:14:37 +0700

    carousel_done

--- dcom/apps/ngshop/src/app/pages/home-page/home-page.component.ts
+++ dcom/apps/ngshop/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
     selector: 'dcom-home-page',
     templateUrl: './home-page.component.html'
@@ -8,4 +7,23 @@ export class HomePageComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
+    // eslint-disable-next-line @typescript-eslint/member-ordering
+    images = [
+        {
+            imageSrc: 'assets/images/carousel/op dan.webp',
+            imageAlt: 'nature1'
+        },
+        {
+            imageSrc: 'assets/images/carousel/Asus gaming.webp',
+            imageAlt: 'nature2'
+        },
+        {
+            imageSrc: 'assets/images/carousel/xiaomi_p1.webp',
+            imageAlt: 'person1'
+        },
+        {
+            imageSrc: 'assets/images/carousel/matebook-d15.jpg',
+            imageAlt: 'person2'
+        }
+    ];
 }
--- dcom/libs/ui/src/lib/components/carousel/carousel.component.ts
+++ dcom/libs/ui/src/lib/components/carousel/carousel.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { isOnRunEffects } from '@ngrx/effects/src/lifecycle_hooks';
 
+interface carouselImage {
+    imageSrc: string;
+    imageAlt: string;
+}
 @Component({
-  selector: 'ui-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+    selector: 'ui-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: []
 })
 export class CarouselComponent implements OnInit {
+    constructor() {}
+    @Input() images: carouselImage[] = [];
+    @Input() indicators = true;
+    @Input() controls = true;
+    @Input() autoSlide = false;
+    @Input() slideInterval = 4000; // Default 3s slide
+    selectedIndex = 0;
 
-  constructor() { }
+    ngOnInit(): void {
+        if (this.autoSlide) {
+            this.autoSlideImage();
+        }
+    }
 
-  ngOnInit(): void {
-  }
-
+    //Change Slide in 3 seconds
+    autoSlideImage(): void {
+        setInterval(() => {
+            this.onNextClick();
+        }, this.slideInterval);
+    }
+    // set index of image on dot/indicator click
+    selectImage(index: number): void {
+        this.selectedIndex = index;
+    }
+    onPrevClick(): void {
+        if (this.selectedIndex === 0) {
+            this.selectedIndex = this.images.length - 1;
+        } else {
+            this.selectedIndex--;
+        }
+    }
+    onNextClick(): void {
+        if (this.selectedIndex === this.images.length - 1) {
+            this.selectedIndex = 0;
+        } else {
+            this.selectedIndex++;
+        }
+    }
 }
